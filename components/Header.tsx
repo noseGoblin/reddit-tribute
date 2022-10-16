2afd9082adbb06df98771f248165117a7075779b
Jacob Mompean <jaymompean@gmail.com>
2022-10-15 23:15:31 -0500

    basic start page with logo and top header frame

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,28 @@
 import Image from 'next/image';
 import React from 'react';
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+} from '@heroicons/react/24/solid';
+import { StarIcon } from '@heroicons/react/24/outline';
 
 function Header() {
   return (
-    <div className='relative h-10 w-20'>
-      <Image
-        objectFit='contain'
-        src='https://www.redditinc.com/assets/images/site/reddit-logo.png'
-        layout='fill'
-      />
+    <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
+      <div className='relative h-10 w-20 flex-shrink-0'>
+        <Image
+          objectFit='contain'
+          src='https://www.redditinc.com/assets/images/site/reddit-logo.png'
+          layout='fill'
+        />
+      </div>
+
+      <div className='mx-7 flex items-center xl:min-w-[300px]'>
+        <HomeIcon className='h-5 w-5' />
+        <p className='ml-2 hidden flex-1'>Home</p>
+        <ChevronDownIcon className='h-5 w-5' />
+      </div>
     </div>
   );
 }
