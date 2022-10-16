a03f53c30f8b70551234a113f28321d80e15bdda
Jacob Mompean <jaymompean@gmail.com>
2022-10-16 16:35:02 -0500

    move header to app template, make sticky, and add site background color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
@@ -9,8 +8,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Reddit 2.0 Clone</title>
       </Head>
-
-      <Header />
     </div>
   );
 };
