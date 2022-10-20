f19da5babc4812a49320c1c795fa012bb3f3c591
Jacob Mompean <jaymompean@gmail.com>
2022-10-19 22:52:29 -0500

    add image & link icons

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,12 +1,13 @@
 import { useSession } from 'next-auth/react';
 import React from 'react';
 import Avatar from './Avatar';
+import { LinkIcon, PhotoIcon } from '@heroicons/react/24/outline';
 
 function PostBox() {
   const { data: session } = useSession();
 
   return (
-    <form>
+    <form className='stick top-16 z-50 rounded-md border border-gray-300 bg-white p-2'>
       <div className='flex items-center space-x-3'>
         <Avatar />
 
@@ -18,6 +19,9 @@ function PostBox() {
             session ? 'Create a post by entering a title!' : 'Sign in to post'
           }
         />
+
+        <PhotoIcon className={`h-6 cursor-pointer text-gray-300`} />
+        <LinkIcon className='h-6 text-gray-300' />
       </div>
     </form>
   );
