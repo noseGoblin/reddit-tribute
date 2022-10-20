02e85ce3da3413c1cebe791f89e913889a57b4ea
Jacob Mompean <jaymompean@gmail.com>
2022-10-19 23:07:12 -0500

    add react hook form & trigger form popout on text input

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -2,9 +2,23 @@ import { useSession } from 'next-auth/react';
 import React from 'react';
 import Avatar from './Avatar';
 import { LinkIcon, PhotoIcon } from '@heroicons/react/24/outline';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreddit: string;
+};
 
 function PostBox() {
   const { data: session } = useSession();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
 
   return (
     <form className='stick top-16 z-50 rounded-md border border-gray-300 bg-white p-2'>
@@ -12,6 +26,7 @@ function PostBox() {
         <Avatar />
 
         <input
+          {...register('postTitle', { required: true })}
           disabled={!session}
           className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
           type='text'
@@ -23,6 +38,21 @@ function PostBox() {
         <PhotoIcon className={`h-6 cursor-pointer text-gray-300`} />
         <LinkIcon className='h-6 text-gray-300' />
       </div>
+
+      {!!watch('postTitle') && (
+        <div className='flex flex-col py-2'>
+          {/* Body */}
+          <div className='flex items-center px-2'>
+            <p className='min-w-[90px]'>Body</p>
+            <input
+              className='m-2 flex-1 bg-blue-50 p-2 outline-none'
+              {...register('postBody')}
+              type='text'
+              placeholder='Text (optional)'
+            />
+          </div>
+        </div>
+      )}
     </form>
   );
 }
