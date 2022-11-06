f2ff57700f1ca467ca66bccfcdf92daaf8edb217
Jacob Mompean <jaymompean@gmail.com>
2022-11-06 01:13:17 -0600

    add Post component to feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -7,7 +7,13 @@ function Feed() {
 
   const posts: Post[] = data?.getPostList;
 
-  return <div>Hello</div>;
+  return (
+    <div>
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default Feed;
