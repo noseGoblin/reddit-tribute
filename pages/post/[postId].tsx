db08449d4498e9e9505c509c197d9f025e62a603
Jacob Mompean <jaymompean@gmail.com>
2022-11-20 20:45:34 -0600

    add loader for post during fetch (alternative to prerendering); add comments section to form

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import React from 'react';
 import Post from '../../components/Post';
@@ -6,6 +7,8 @@ import { GET_POSTS_BY_POST_ID } from '../../graphql/queries';
 
 function PostPage() {
   const router = useRouter();
+  const { data: session } = useSession();
+
   const { data } = useQuery(GET_POSTS_BY_POST_ID, {
     variables: {
       post_id: router.query.postId,
@@ -15,8 +18,31 @@ function PostPage() {
   const post: Post = data?.getPostListByPostId;
 
   return (
-    <div>
+    <div className='mx-auto my-7 max-w-5xl'>
       <Post post={post} />
+
+      <div className='-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16'>
+        <p className='text-sm'>
+          Comment as <span className='text-red-500'>{session?.user?.name}</span>
+        </p>
+
+        <form className='flex max-w-5xl flex-col'>
+          <textarea
+            disabled={!session}
+            className='h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50'
+            placeholder={
+              session ? 'What are your thoughts?' : 'Please sign in to comment'
+            }
+          />
+
+          <button
+            type='submit'
+            className='rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200'
+          >
+            Comment
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
