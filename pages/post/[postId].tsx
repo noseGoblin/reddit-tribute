2980018c34a280b810cc557ffcfbc687d95df7b1
Jacob Mompean <jaymompean@gmail.com>
2022-11-20 20:29:32 -0600

    add post query to dynamic route page

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,10 +1,24 @@
+import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
 import React from 'react';
+import Post from '../../components/Post';
+import { GET_POSTS_BY_POST_ID } from '../../graphql/queries';
 
 function PostPage() {
   const router = useRouter();
+  const { data } = useQuery(GET_POSTS_BY_POST_ID, {
+    variables: {
+      post_id: router.query.postId,
+    },
+  });
 
-  return <div>PostPage</div>;
+  const post: Post = data?.getPostListByPostId;
+
+  return (
+    <div>
+      <Post post={post} />
+    </div>
+  );
 }
 
 export default PostPage;
