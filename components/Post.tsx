0d851d18fec04372b59c9e9e73230a4a42b4734b
Jacob Mompean <jaymompean@gmail.com>
2022-11-20 22:53:10 -0600

    Add displayVotes functionality

--- components/Post.tsx
+++ components/Post.tsx
@@ -72,6 +72,22 @@ function Post({ post }: Props) {
     setVote(vote);
   }, [data]);
 
+  const displayVotes = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId;
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1) : (total -= 1)),
+      0
+    );
+
+    if (votes.length === 0) return 0;
+
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1;
+    }
+
+    return displayNumber;
+  };
+
   if (!post)
     return (
       <div className='flex w-full items-center justify-center p-10 text-xl'>
@@ -90,7 +106,7 @@ function Post({ post }: Props) {
               vote && 'text-blue-400'
             }`}
           />
-          <p className='text-xs font-bold text-black'>0</p>
+          <p className='text-xs font-bold text-black'>{displayVotes(data)}</p>
           <ArrowDownIcon
             onClick={() => upVote(false)}
             className={`voteButtons hover:text-red-400 ${
