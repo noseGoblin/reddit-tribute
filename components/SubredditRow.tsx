ae18afb063176a51ec8d7b08a24174ebef2511bd
Jacob Mompean <jaymompean@gmail.com>
2022-11-27 21:25:37 -0600

    style SubredditRow container

--- components/SubredditRow.tsx
+++ components/SubredditRow.tsx
@@ -7,7 +7,9 @@ type Props = {
 };
 
 function SubredditRow({ index, topic }: Props) {
-  return <div>SubredditRow</div>;
+  return (
+    <div className='flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b'></div>
+  );
 }
 
 export default SubredditRow;
