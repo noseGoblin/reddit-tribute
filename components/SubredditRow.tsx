273227215ef09a9f5d1fdfeef3838d215f6e7060
Jacob Mompean <jaymompean@gmail.com>
2022-11-27 21:40:38 -0600

    add component logic to SubredditRow to render subreddit list

--- components/SubredditRow.tsx
+++ components/SubredditRow.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ChevronUpIcon } from '@heroicons/react/outline';
+import { ChevronUpIcon } from '@heroicons/react/24/outline';
+import Avatar from './Avatar';
 
 type Props = {
   topic: string;
@@ -8,7 +9,12 @@ type Props = {
 
 function SubredditRow({ index, topic }: Props) {
   return (
-    <div className='flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b'></div>
+    <div className='flex items-center space-x-2 border-t bg-white px-4 py-2 last:rounded-b'>
+      <p>{index + 1}</p>
+      <ChevronUpIcon className='h-4 w-4 flex-shrink-0 text-green-400' />
+      <Avatar seed={`/subreddit/${topic}`} />
+      <p>r/{topic}</p>
+    </div>
   );
 }
 
