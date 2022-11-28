e981720f0c957934fb0405b11422525b3a4cc013
Jacob Mompean <jaymompean@gmail.com>
2022-11-27 21:11:27 -0600

    add query for subreddits to home & create component to list them

---
+++ components/SubredditRow.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {
+  topic: string;
+  index: number;
+};
+
+function SubredditRow({ index, topic }: Props) {
+  return <div>SubredditRow</div>;
+}
+
+export default SubredditRow;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from '@apollo/client';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Feed from '../components/Feed';
 import PostBox from '../components/PostBox';
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries';
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    },
+  });
+  const subreddits: Subreddit[] = data?.getSubredditListLimit;
+
   return (
     <div className='my-7 max-w-5xl mx-auto'>
       <Head>
