5ab89512e281c5dc7a447e940031e55e0829c220
Jacob Mompean <jaymompean@gmail.com>
2022-11-27 21:18:52 -0600

    add SubredditRow component & key mapping

--- components/SubredditRow.tsx
+++ components/SubredditRow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ChevronUpIcon } from '@heroicons/react/outline';
 
 type Props = {
   topic: string;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Feed from '../components/Feed';
 import PostBox from '../components/PostBox';
+import SubredditRow from '../components/SubredditRow';
 import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries';
 
 const Home: NextPage = () => {
@@ -27,7 +28,11 @@ const Home: NextPage = () => {
         <div className='sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline'>
           <p className='text-md mb-1 p-4 pb-3 font-bold'>Top Communities</p>
 
-          <div>{/* List subreddits */}</div>
+          <div>
+            {subreddits?.map((subreddit, i) => (
+              <SubredditRow key={subreddit.id} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
